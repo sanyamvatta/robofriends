413e05dfa0fda1d240a63889bff5a154549be03c
Sanyam vatta <sanyamvatta4@gmail.com>
2023-03-03 13:08:56 +0530

    Implemented hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,38 @@
-import React from "react"
+import React,{useState,useEffect} from "react"
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from "../components/ErrorBoundary";
 
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      robots: [],
-      searchfield : ''
-    }
+
+
+
+function App() {
+
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchField] = useState('');
+
+  const onSearchChange=(e)=>{
+    setSearchField(e.target.value);
   }
 
-  onSearchChange=(e)=>{
-    this.setState({searchfield: e.target.value})
+  const filteredRobots = robots.filter(robot=>{
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase())
   }
+  )
 
-  render(){
-    const {robots,searchChange} = this.state
-    const filteredRobots = robots.filter(robot=>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    }
-    )
-    if (!(robots.length)){
-      return <h1>Loading...</h1>
-    }else{
-      return(
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      res.json().then((data)=>{ setRobots(data) })
+    })
+  },[])
+
+  return !robots.length ? <h1>Loading...</h1> : (
         <div className="tc">
           <h1 className="f1 white">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots}/>
@@ -38,15 +40,8 @@ class App extends React.Component {
           </Scroll>
         </div>
       )
-    }
-    
-  }
-
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res)=>{
-      res.json().then((data)=>{ this.setState({ robots: data}) })
-    })
-  }
 }
+    
+
+
 export default App
